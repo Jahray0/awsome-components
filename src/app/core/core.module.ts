e5097ef7df321c29a404c16f4f52e3fc15e79d74
Jahray0 <jaegle.maxime@gmail.com>
2023-07-02 17:53:43 +0200

    header + routing

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { SharedModule } from '../shared/shared.module';
+import { Router, RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ],
+
+  exports: [
+    HeaderComponent
+  ]
+
+})
+export class CoreModule { }
