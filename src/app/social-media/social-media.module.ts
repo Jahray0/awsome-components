fd42a368f31883f0285385aafb550f5a25e71697
Jahray0 <jaegle.maxime@gmail.com>
2023-07-04 10:23:50 +0200

    v 1.0.1.1 resolver

--- src/app/social-media/social-media.module.ts
+++ src/app/social-media/social-media.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SocialMediaRoutingModule } from './social-media-routing.module';
+import { PostsService } from './services/posts.service';
+import { PostsResolver } from './resolvers/posts.resolver';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PostListComponent
+  ],
   imports: [
     CommonModule,
     SocialMediaRoutingModule
+  ],
+  providers: [
+    PostsService,
+    PostsResolver
   ]
 })
 export class SocialMediaModule { }
