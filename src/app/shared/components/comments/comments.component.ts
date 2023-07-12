7cff81df2a0ec11852b86a17592fe1e122ede340
Jahray0 <jaegle.maxime@gmail.com>
2023-07-12 16:00:41 +0200

    v 1.4.4 pipe time-ago

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,35 +1,32 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
-import { Comment } from "../../../core/models/comment.model";
-import { FormBuilder, FormControl, Validators } from "@angular/forms";
-
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Comment } from '../../../core/models/comment.model';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: "app-comments",
-  templateUrl: "./comments.component.html",
-  styleUrls: ["./comments.component.scss"]
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.scss'],
 })
-
 export class CommentsComponent implements OnInit {
-
   @Input() comments!: Comment[];
   @Output() newComment = new EventEmitter<string>();
 
-  commentCtrl!: FormControl
+  commentCtrl!: FormControl;
 
-  constructor(private formBuilder: FormBuilder){ }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void { 
-    this.commentCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)]);
+  ngOnInit(): void {
+    this.commentCtrl = this.formBuilder.control('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]);
   }
 
-  onLeaveComment()  {
-
+  onLeaveComment() {
     if (this.commentCtrl.invalid) {
       return;
     }
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
-
-
-   }
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,9 +5,10 @@ import { MaterialModule } from './material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ShortenPipe } from './pipes/shorten.pipe';
 import { UsernamePipe } from './pipes/username.pipe';
+import { TimeAgoPipe } from './pipes/time-ago.pipe';
 
 @NgModule({
-  declarations: [CommentsComponent, ShortenPipe, UsernamePipe],
+  declarations: [CommentsComponent, ShortenPipe, UsernamePipe, TimeAgoPipe],
   imports: [CommonModule, MaterialModule, ReactiveFormsModule],
 
   exports: [
@@ -16,6 +17,7 @@ import { UsernamePipe } from './pipes/username.pipe';
     ReactiveFormsModule,
     ShortenPipe,
     UsernamePipe,
+    TimeAgoPipe,
   ],
 })
 export class SharedModule {}
