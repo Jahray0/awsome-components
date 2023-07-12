9885274b277a538cd1ef915bc63e5d228f963450
Jahray0 <jaegle.maxime@gmail.com>
2023-07-12 17:27:39 +0200

    v1.5.2 Directives avec param

--- src/app/shared/directives/highlight.directive.ts
+++ src/app/shared/directives/highlight.directive.ts
@@ -2,6 +2,7 @@ import {
   AfterViewInit,
   Directive,
   ElementRef,
+  HostListener,
   Input,
   Renderer2,
 } from '@angular/core';
@@ -19,5 +20,17 @@ export class HighlightDirective implements AfterViewInit {
 
   setBackgroundColor(color: string) {
     this.renderer.setStyle(this.el.nativeElement, 'background-color', color);
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBackgroundColor('lightgreen');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBackgroundColor(this.color);
+  }
+
+  @HostListener('click') onClick() {
+    this.color = 'lightgreen';
   }
 }
