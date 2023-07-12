a73157708b56cff2c1dbe1bf5afcd354a86d4430
Jahray0 <jaegle.maxime@gmail.com>
2023-07-12 16:39:56 +0200

    v 1.5.1 directives

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,9 +6,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ShortenPipe } from './pipes/shorten.pipe';
 import { UsernamePipe } from './pipes/username.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
-  declarations: [CommentsComponent, ShortenPipe, UsernamePipe, TimeAgoPipe],
+  declarations: [
+    CommentsComponent,
+    ShortenPipe,
+    UsernamePipe,
+    TimeAgoPipe,
+    HighlightDirective,
+  ],
   imports: [CommonModule, MaterialModule, ReactiveFormsModule],
 
   exports: [
@@ -18,6 +25,7 @@ import { TimeAgoPipe } from './pipes/time-ago.pipe';
     ShortenPipe,
     UsernamePipe,
     TimeAgoPipe,
+    HighlightDirective,
   ],
 })
 export class SharedModule {}
